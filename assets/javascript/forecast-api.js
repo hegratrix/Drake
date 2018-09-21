20dfb2cee7faf8dffcbfe73d06882df4e46f13b1
Tanya Valdez <TValdez@digmap.com>
2018-09-21 09:39:47 -0700

    Add filter to cityForecasts

--- assets/javascript/forecast-api.js
+++ assets/javascript/forecast-api.js
@@ -18,22 +18,38 @@ $(".btn").on("click", function (event) {
   let url1 =
     "https://api.apixu.com/v1/forecast.json?key=b44ed5063f3342b280513045181409&q=" +
     city1 +
-    "&days=" +
-    duration;
+    "&days=10"
+
+  //changed the last part of the api to include "10" instead of duration. They say they provide *up to 10days of forecast
+  // so I am pulling the max data in and stepping into the correct dates below.
 
   let url2 =
     "https://api.apixu.com/v1/forecast.json?key=b44ed5063f3342b280513045181409&q=" +
     city2 +
-    "&days=" +
-    duration;
+    "&days=10"
+
+  //changed the last part of the api to include "10" instead of duration. They say they provide *up to 10days of forecast
+  // so I am pulling the max data in and stepping into the correct dates below.
 
   Promise.all([$.get(url1), $.get(url2)])
     .then(function (results) {
       console.log(results);
+      console.log(duration);
+      console.log(toDate);
+      console.log(fromDate);
+      //added filter here so it would start pulling from the correct dates
+      const city1Forecasts = results[0].forecast.forecastday.filter(
+        day => day.date >= fromDate
+      );
 
-      const city1Forecasts = results[0].forecast.forecastday;
-      const city2Forecasts = results[1].forecast.forecastday;
-
+      console.log(city1Forecasts);
+      //added this console log for city1Forecasts to see how much data we are getting back from the api since there is a limit and we dont always know
+      console.log(results[0].forecast.forecastday);
+      //added filter here so it would start pulling from the correct dates
+      const city2Forecasts = results[1].forecast.forecastday.filter(
+        day => day.date >= fromDate
+      );
+      console.log(city2Forecasts);
       for (let i = 0; i < duration; i++) {
         const city1DayMax = city1Forecasts[i].day.maxtemp_f;
         const city2DayMax = city2Forecasts[i].day.maxtemp_f;
@@ -52,15 +68,22 @@ $(".btn").on("click", function (event) {
 
         const city2Condition = city2Forecasts[i].day.condition.text;
         const city2Icon = city2Forecasts[i].day.condition.icon;
-        console.log(`Day ${i + 1}: ${city2Condition}`)
-        console.log(`Day ${i + 1}: https:${city2Icon}`)
+        console.log(`Day ${i + 1}: ${city2Condition}`);
+        console.log(`Day ${i + 1}: https:${city2Icon}`);
 
         $("#forecast").append(`
         <div>Day ${i + 1}: ${Math.round(maxDiff)} \xB0 F </div>`);
-        $("#forecast").append(`<div>Day ${i + 1}: ${Math.round(humDiff)}%</div>`);
-        $("#forecast").append(`<div>Day ${i + 1}: ${Math.round(winDiff)}mph</div>`);
+        $("#forecast").append(
+          `<div>Day ${i + 1}: ${Math.round(humDiff)}%</div>`
+        );
+        $("#forecast").append(
+          `<div>Day ${i + 1}: ${Math.round(winDiff)}mph</div>`
+        );
         $("#forecast").append(`<div>Day ${i + 1}: ${city2Condition}</div>`);
-        $("#forecast").append(`<div>Day ${i + 1}: <img id="weatherIcon" src="https:${city2Icon}" alt=weatherIcon></div>`);
+        $("#forecast").append(
+          `<div>Day ${i +
+          1}: <img id="weatherIcon" src="https:${city2Icon}" alt=weatherIcon></div>`
+        );
       }
     })
     .catch(function (err) {
