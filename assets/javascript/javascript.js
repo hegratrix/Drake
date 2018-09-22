16cb7588be1f06d8e420a0479949eff6950cb7ea
hegratrix <gratrixmomma@yahoo.com>
2018-09-22 13:51:18 -0700

    sign in modal

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -21,3 +21,23 @@ function changeImg() {
 }
 
 window.onload = changeImg()
+
+// sign up pop up window
+var modal = document.getElementById('myModal')
+var btn = document.getElementById("myBtn")
+var span = document.getElementsByClassName("close")[0]
+
+btn.onclick = function() {
+    event.preventDefault()
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
