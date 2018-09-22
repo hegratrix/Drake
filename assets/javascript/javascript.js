048c24c8e36bac2afdd7e11c5b3641419ac8433e
hegratrix <gratrixmomma@yahoo.com>
2018-09-22 14:43:30 -0700

    log in modal

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -23,18 +23,23 @@ function changeImg() {
 window.onload = changeImg()
 
 // Get the modal
-var modal = document.getElementById('myModal');
-
+var modal = document.getElementById('myModalSignUp');
+var modal2 = document.getElementById('myModalLogIn');
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtnSignUp");
+var btn2 = document.getElementById("myBtnLogIn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var span2 = document.getElementsByClassName("close2")[0];
 // When the user clicks the button, open the modal 
    btn.onclick = function() {
     event.preventDefault()
     modal.style.display = "block";
+}
+btn2.onclick = function() {
+    event.preventDefault()
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -42,9 +47,15 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    } else if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
